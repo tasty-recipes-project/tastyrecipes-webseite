39b19cd33d862119c8c36146eac4f8200b3d66be
JohnBoy702 <jonas.grafe@web.de>
2021-02-01 14:42:46 +0100

    Mein Profil Höhe geändert

--- includes/dbc.inc.php
+++ includes/dbc.inc.php
@@ -5,10 +5,10 @@
     //BenutzerDB
      $servername="localhost";
      $dBUsername = "root";
-     //$dBPassword = "root";
+     $dBPassword = "root";
      $dBName = "tastyrecipes";
 
-     $con = mysqli_connect($servername, $dBUsername, '', $dBName);
+     $con = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
      if (!$con) {
          die("Verbindung Fehlgeschlagen" .mysqli_connect_error());
