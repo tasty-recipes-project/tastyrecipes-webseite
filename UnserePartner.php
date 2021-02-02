a49259d20dd3afa40e8139e92d2a5c000f934896
pa-maker <patrick.nuhn@informatik.hs-fulda.de>
2021-02-02 12:25:11 +0100

    Create UnserePartner.php

---
+++ UnserePartner.php
@@ -0,0 +1,15 @@
+<?php
+    require('header.php')
+?>
+
+<html>
+
+	<div class="page">
+	
+	<div class="gba-kachel gba-kachel--xs">
+	<div class="padding-all-unit">
+	<div class="gba-kachel__titel">Titel</div>
+	<div class="gba-kachel__body">Text</div>
+	<div class="gba-kachel__footer"><a href="" class="gba-button gba-button--standard">mehr</a></div>
+	
+</html>	
