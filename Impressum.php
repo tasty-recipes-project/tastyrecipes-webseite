2f99bacf041fff6c731d11ca373911acc18b0180
pa-maker <75268352+pa-maker@users.noreply.github.com>
2021-02-02 16:36:10 +0100

    Update Impressum.php

--- Impressum.php
+++ Impressum.php
@@ -1,5 +1,5 @@
 <?php
- session_start();
+ include('header.php')
 ?>
 
 <!Doctype html>
@@ -30,3 +30,7 @@
 	<p style="text-align: justify;">Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
 	<p style="text-align: justify;">Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
 </html>
+
+<?php
+ include('footer.php')
+?>
