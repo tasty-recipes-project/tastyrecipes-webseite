5f249cca8b0e33c0bd69fd546540c0f7f59f85a5
JohnBoy702 <jonas.grafe@web.de>
2021-02-03 18:12:15 +0100

    aboutUs angepasst

--- aboutUs.php
+++ aboutUs.php
@@ -11,52 +11,68 @@
 
 	<body>
 		<main class="main_content">
-		<div class="">
+			<div class="aboutUS_content">
+				<div class="aboutUs_img">
+					<p><img id="bildaboutus" src="images/bild.jpg" width="100%"></p>
+				</div>
 
+				<div class="aboutUs_info">
+					<h1><center>Über Uns</center></h1>
+					<p></p>
+					<p>
+						<center>
+							<span style="font-weight:bold"; style="font-family:'Times New Roman',Times,serif">
+								Alles, was wir tun, tun wir mit Leidenschaft!
+							</span>
+						</center>
+					</p>
+					<p>
+					Was uns jeden Tag weiter wachsen lässt, ist der Glaube daran, dass füreinander kochen und gemeinsam essen viel glücklicher macht. Unser oberstes Ziel ist es Menschen für gutes Essen zu begeistern und sie auch zusammenzubringen.
+					Als wir im 5. Semester zusammen kamen und die tolle Idee hatten, eine Internetseite zu erstellen, auf der Menschen sich untereinander austauschen können und die genauso eine große Leidenschaft für gutes Essen wie wir haben, konnten wir noch gar nicht damit rechnen, dass daraus eine so große Community wird.
+					</p>
+					<p>
+					"Jede Jeck is anders", sagt man im Rheinland. So gehen wir mit tastyrecipes auf die persönlichen Vorlieben jedes/r Nutzers/in ein und bieten personalisierte Rezepte, die jedem einen leichten und dennoch geschmackhaften Zugang zu gutem Essen erleichtert. Wir von tastyrecipes sind der Meinung, dass es nicht jeden Tag Fast Food sein muss, um glücklich zu sein.
+					</p>
+				</div>
 
-		<div class="aboutUs_img">
-
-		<p><img id="bildaboutus" src="images/bild.jpg" width="100%"></p>
-
-		</div>
-		<h1><center>Über Uns</center></h1>
-		<p></p>
-		<p>
-		<center>
-		<span style="font-weight:bold"; style="font-family:'Times New Roman',Times,serif">
-		Alles, was wir tun, tun wir mit Leidenschaft!
-		</span>
-		</center>
-		</p>
-		<p>
-		Was uns jeden Tag weiter wachsen lässt, ist der Glaube daran, dass füreinander kochen und gemeinsam essen viel glücklicher macht. Unser oberstes Ziel ist es Menschen für gutes Essen zu begeistern und sie auch zusammenzubringen.
-		Als wir im 5. Semester zusammen kamen und die tolle Idee hatten, eine Internetseite zu erstellen, auf der Menschen sich untereinander austauschen können und die genauso eine große Leidenschaft für gutes Essen wie wir haben, konnten wir noch gar nicht damit rechnen, dass daraus eine so große Community wird.
-		</p>
-		<p>
-		"Jede Jeck is anders", sagt man im Rheinland. So gehen wir mit tastyrecipes auf die persönlichen Vorlieben jedes/r Nutzers/in ein und bieten personalisierte Rezepte, die jedem einen leichten und dennoch geschmackhaften Zugang zu gutem Essen erleichtert. Wir von tastyrecipes sind der Meinung, dass es nicht jeden Tag Fast Food sein muss, um glücklich zu sein.
-		</p>
-		<p></p>
-		<br></br>
-		<h1><center>Unser Team</center></h1>
-		<p><center>Unser Team besteht aus 4 Mitgliedern, die unterschiedliche Tätigkeiten wahrnehmen.</center></p>
-		<p></p>
-		<div class="row">
-		<div class="col-lg-12">
-		<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
-		<p>Olivia Dornberger</p>
-		<p></p>
-		<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
-		<p>Jonas Grafe</p>
-		<p></p>
-		<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
-		<p>Florian Rabe</p>
-		<p></p>
-		<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
-		<p>Patrick Nuhn</p>
-		</div>
-		</div>
-		<br></br>
-		</div>
+				<div class="aboutUs_team">
+						<h1><center>Unser Team</center></h1>
+						<p><center>Unser Team besteht aus 4 Mitgliedern, die unterschiedliche Tätigkeiten wahrnehmen.</center></p>
+						<br>
+						<div class="bild1">
+							<center>
+								<figure>
+									<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
+									<figcaption>Olivia Dornberger</figcaption>
+								</figure>
+							</center>
+						</div>
+						<div class="bild2">
+							<center>
+								<figure>
+									<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
+									<figcaption>Florian Rabe</figcaption>
+								</figure>
+							</center>
+						</div>
+						<div class="bild3">
+							<center>
+								<figure>
+									<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
+									<figcaption>Patrick Nuhn</figcaption>
+								</figure>
+							</center>
+						</div>
+						<div class="bild4">
+							<center>
+								<figure>
+									<img class="linksaboutus" src="images/bild.jpg" width="220" height="180" border="0" alt="Banner">
+									<figcaption>Jonas Grafe</figcaption>
+								</figure>
+							</center>
+						</div>
+				</div>
+			</div>
 		</main>
 	</body>
 </html>
