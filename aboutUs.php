20da09b50f549fc48a52c40043dcf968c73d1042
pa-maker <75268352+pa-maker@users.noreply.github.com>
2021-02-03 10:05:29 +0100

    Update aboutUs.php

--- aboutUs.php
+++ aboutUs.php
@@ -11,16 +11,21 @@
 	</head>
 	<body>
 		<h1>Willkommen bei Tasty Recipes</h1>
-		
+		<p></p>
 		<p>
-			Unser Ziel ist es, unsere Kunden mit einer Vielzahl von Rezepten zu begeistern. 
-			Ob einfache und schnelle Rezepte oder aufwendige Mahlzeiten.
-			Bei uns werden sie fündig!
+		<center>
+		<span style="font-weight:bold"; style="font-family:'Times New Roman',Times,serif">
+		Alles, was wir tun, tun wir mit Leidenschaft!
+		</span>	
+		</center>
 		</p>
-		
 		<p>
-		Diese Webseite entstand aufgrund des Bachelor Projekts von Olivia Dornberger, Jonas Grafe, Florian Rabe und Patrick Nuhn.
+		Was uns jeden Tag weiter wachsen lässt, ist der Glaube daran, dass füreinander kochen und gemeinsam essen viel glücklicher macht. Unser oberstes Ziel ist es Menschen für gutes Essen zu begeistern und sie auch zusammenzubringen. 
+		Als wir im 5. Semester zusammen kamen und die tolle Idee hatten, eine Internetseite zu erstellen, auf der Menschen sich untereinander austauschen können und die genauso eine große Leidenschaft für gutes Essen wie wir haben, konnten wir noch gar nicht damit rechnen, dass daraus eine so große Community wird.
 		</p>
+		<p>
+		"Jede Jeck is anders", sagt man im Rheinland. So gehen wir mit tastyrecipes auf die persönlichen Vorlieben jedes/r Nutzers/in ein und bieten personalisierte Rezepte, die jedem einen leichten und dennoch geschmackhaften Zugang zu gutem Essen erleichtert. Wir von tastyrecipes sind der Meinung, dass es nicht jeden Tag Fast Food sein muss, um glücklich zu sein.
+		</p>	
 	</body>
 </html>
 </main>
