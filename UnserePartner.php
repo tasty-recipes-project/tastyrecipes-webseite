09224564599a99eeb8b68a115173f77f8f345322
pa-maker <75268352+pa-maker@users.noreply.github.com>
2021-02-05 15:32:52 +0100

    Update UnserePartner.php

--- UnserePartner.php
+++ UnserePartner.php
@@ -7,7 +7,7 @@
 	<head>
 		<title>Unsere Partner</title>
 		<meta charset="utf-8">
-        <link link rel="stylesheet" href="styles.css">
+        <link link rel="stylesheet" href="partner.css">
 	</head>
 	
 	<body>
