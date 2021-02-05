7620e1ce3a2bef328024e55488bde7c989fcbb6d
pa-maker <75268352+pa-maker@users.noreply.github.com>
2021-02-05 15:23:58 +0100

    Update UnserePartner.php

--- UnserePartner.php
+++ UnserePartner.php
@@ -1,15 +1,56 @@
 <?php
     require('header.php')
 ?>
-
+<br></br>
+<!DOCTYPE html>
 <html>
-
-	<div class="page">
+	<head>
+		<title>Unsere Partner</title>
+		<meta charset="utf-8">
+        <link link rel="stylesheet" href="probe.css">
+	</head>
 	
-	<div class="gba-kachel gba-kachel--xs">
-	<div class="padding-all-unit">
-	<div class="gba-kachel__titel">Titel</div>
-	<div class="gba-kachel__body">Text</div>
-	<div class="gba-kachel__footer"><a href="" class="gba-button gba-button--standard">mehr</a></div>
-	
+	<body>
+		   <details>
+				<summary>
+					<span style="font-size:25px";>Unsere Partner</span>
+				</summary>
+				<table>
+					<tr>
+						<td><a href="https://www.chefkoch.de/"><img class="linksaboutus" src="imagesPartner/chefkoch.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></td></a>
+						<td><a href="https://www.essen-und-trinken.de/"><img class="linksaboutus" src="imagesPartner/recipe.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></td></a>
+						<th><a href="https://www.kochbar.de/"><img class="linksaboutus" src="imagesPartner/kochbar.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></th></a>
+						<th><a href="https://www.daskochrezept.de/"><img class="linksaboutus" src="imagesPartner/kochrezept.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></th></a>
+						<td><a href="https://eatsmarter.de/"><img class="linksaboutus" src="imagesPartner/eatsmarter.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></td></a>
+					</tr>
+					<tr>
+						<td><a href="https://www.aldi-nord.de/"><img class="linksaboutus" src="imagesPartner/aldi.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></td></a>
+						<td><a href="https://www.lidl.de/"><img class="linksaboutus" src="imagesPartner/lidl.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></td></a>
+						<th><a href="https://www.netto-online.de/"><img class="linksaboutus" src="imagesPartner/netto.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></th></a>
+						<th><a href="https://www.edeka.de/ernaehrung/bewusste-ernaehrung-mit-edeka.jsp"><img class="linksaboutus" src="imagesPartner/edeka.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></th></a>
+						<td><a href="https://www.penny.de/"><img class="linksaboutus" src="imagesPartner/penny.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></td></a>
+					</tr>
+				</table>
+			</details>
+		<p></p>
+			<details>
+				<summary>
+					<span style="font-size:25px">Kooperationen</span>
+				</summary>
+				<p>
+				<table>
+					<tr>
+						<th><a href="https://www.weightwatchers.com/de/"><img class="linksaboutus" src="imagesPartner/ww.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></a>
+						<th><a href="https://www.gesundheit.de/"><img class="linksaboutus" src="imagesPartner/gesundheit.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></a>
+						<th><a href="https://www.netdoktor.de/"><img class="linksaboutus" src="imagesPartner/netdoktor.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></a>
+						<th><a href="https://www.shop-apotheke.com/"><img class="linksaboutus" src="imagesPartner/shop-apotheke.jpg" width="180" height="150" border="0" style="border-style: solid; border-width: 2px; border-color: black;"/></th></a>
+					</tr>
+				</table>
+				</p>
+			</details>
+	</body>
 </html>	
+<br></br>
+<?php
+	require('footer.php')
+?>
